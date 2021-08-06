43990eff57446ce562a420de0c6a25aca2ffc82f
Vedha <vedhavel.1234@gmail.com>
2021-08-06 06:55:45 +0530

    [Vedhashni] Updated the file

---
+++ AddressBookLINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book using linq");
+        }
+    }
+}
